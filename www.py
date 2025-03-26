3311857f1d2fecad9382e50f14a0beb798672347
unknown <229154@TECH.WYB.AC.LK>
2025-03-26 20:22:08 +0530

    first commit

---
+++ www.py
@@ -0,0 +1,13 @@
+num1=int(input("enter first number="))
+num2=int(input("enter second number="))
+num3=int(input("enter third number="))
+ 
+if(num1>num2):
+    if(x>z):
+        print(num1)
+else:
+    if(num2>num3):
+        print(num2)
+    else:
+        print(num3)    
+                
